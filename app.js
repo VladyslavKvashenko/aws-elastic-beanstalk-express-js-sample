595bab423ce4717577aa65d6c5b9b5af851aa20d
Vladylsav <kvashenko.vladyslav@gmail.com>
2022-08-19 19:13:29 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Simple CI/CD project with AWS!'));
+app.get('/', (req, res) => res.send('Simple CI/CD project with AWS! With New stage now'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
